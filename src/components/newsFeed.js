cce2c5e3f45ea050e27e991bacdad0028a5f5b8c
Sam Hill <samuhill@gmail.com>
2018-12-09 05:51:13 -0500

    move newsFeed to use styled-components

--- src/components/newsFeed.js
+++ src/components/newsFeed.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import './newsFeed.css';
+import styled from 'styled-components';
+
+const StyledFeed = styled.div`
+	display: inline-block;
+	background: none;
+`;
+
+const StyledNewsItem = styled.a`
+	display: block;
+	text-decoration: none;
+	background: white;
+	margin-bottom: 1rem;
+	color: black;
+	padding: .5rem;
+	transition: all .1s;
+
+	&:hover {
+		transform: scale(1.05);
+		color: black;
+		text-decoration: none;
+	}
+`;
+
+const StyledTitle = styled.span`
+	display: block;
+	font-size: 2rem;
+	margin-bottom: 1rem;
+`;
+
+const StyledDescription = styled.span`
+	color: grey;
+	font-size: 1.5rem;
+`;
 
 export default class NewsFeed extends Component {
 	buildNewsItem(title, description, href) {
-		return (<a className="news-item" target="_blank" href={href}>
-				<span className="title">{title}</span>
-				<span className="description">{description.replace(/&nbsp;/g, ' ')}</span>
-		</a>)
+		return (
+			<StyledNewsItem target="_blank" href={href}>
+				<StyledTitle>{title}</StyledTitle>
+				<StyledDescription>{description.replace(/&nbsp;/g, ' ')}</StyledDescription>
+		</StyledNewsItem>
+		)
 	};
 
 	render() {
     var searchTerms = Object.keys(this.props.data);
 
 		return (
-				<div className="news-feed">
+				<StyledFeed>
 					  {searchTerms.map((item, index) => {
               return this.props.data[item].map((article, i) => {
 								return this.buildNewsItem(article.title, article.description, article.link)
               })
             })}
-				</div>
+				</StyledFeed>
 		);
 	};
 }
